abab0e7b498424c767e59fcba31dc76593084206
dinhdungz <nguyendinhdung17062001@gmail.com>
2023-06-28 22:05:08 +0700

    update init background

--- init_background.py
+++ init_background.py
@@ -13,11 +13,11 @@ height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 fps =  cap.get(cv2.CAP_PROP_FPS)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, fps, (width,height))
+out = cv2.VideoWriter('output_0.avi', fourcc, fps, (width,height))
 
 # Number frame in init background
 N = 6 
-N_BOI = 20
+N_BOI = 2
 init_background = True
 gen_BOI = True
 #Block coordinates
@@ -75,8 +75,8 @@ def update_model(i, delta_v, mean):
         lr_b[i] = 0.01
     else:
         lr_b[i] = 0.1
-    if lambda_b[i] < 500:
-        lambda_b[i] = (1 - lr_b[i]) * lambda_b[i] + lr_b[i] * delta_v
+    # if lambda_b[i] < 500:
+    #     lambda_b[i] = (1 - lr_b[i]) * lambda_b[i] + lr_b[i] * delta_v
     if proba_mean(mean, BOIs_mean[i][0], BOIs_var[i][0], lr_am[i]) > proba_mean(BOIs_mean[i][0] + 3*BOIs_var[i][0], BOIs_mean[i][0], BOIs_var[i][0], lr_am[i]):
         lr_am[i] = 0.01
     else:
@@ -103,19 +103,19 @@ while cap.isOpened():
         print("Can't receive frame (stream end?). Exiting ...")
         break
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
+    kernel = np.ones((5,5),np.float32)/25
+    img = cv2.filter2D(img,-1,kernel)
     s+=1
     n_o = 0
-
     # Generate ROI and BOI
     if gen_BOI:
-        lanes = ROI.get_lanes(img)
-        lanes_image = ROI.draw_lanes(img, lanes)
+        lanes = ROI.get_lanes(frame)
+        lanes_image = ROI.draw_lanes(frame, lanes)
 
         area = BOI.get_area(lanes)
         area_image = BOI.draw_lanes(lanes_image, area)
 
-        boi_image, BOIs_coor = BOI.get_BOI(area, area_image, 20, 2)
+        boi_image, BOIs_coor = BOI.get_BOI(area, area_image,N_BOI, 2)
         cv2.imshow('lane', boi_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
@@ -130,7 +130,7 @@ while cap.isOpened():
                 BOIs_mean[i][count] = mean
             count +=1
             cv2.putText(frame, 'Init background', (100, 100),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2 )
-            cv2.imshow("video",img)
+            cv2.imshow("video",frame)
             out.write(frame)
         else:
             pas = True
@@ -145,23 +145,23 @@ while cap.isOpened():
                     mean, var = get_mean_variance(img, BOIs_coor[i])
                     BOIs_var[i].append(var)
                     BOIs_mean[i].append(mean)
-                    cv2.putText(frame, 'Init background', (100, 100),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2 )
-                    cv2.imshow("video",img)
-                    out.write(frame)
+            cv2.putText(frame, 'Init background', (100, 100),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2 )
 
             if pas:
                 init_background = False
                 g_mean = np.mean(BOIs_mean, axis = 1)
                 g_var = np.mean(BOIs_var, axis = 1)
-                print("Done init background")
+                print(f"g_mean {g_mean}")
+                print(f"g_var {g_var}")
+                # print("Done init background")
                 cv2.putText(frame, 'Done init background', (100, 100),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2 )
-                cv2.imshow("video",img)
-                out.write(frame)
-
+        cv2.imshow("video",frame)
+        out.write(frame)
     else:
         # Classify object
         positions = []
         for i in range(N_BOI):
+            # cv2.putText(frame, str(i), BOIs_coor[i][0],cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2 )
             mean, var = get_mean_variance(img, BOIs_coor[i])
             delta_v = abs(var - g_var[i])
             p_vb = math.exp(-delta_v/lambda_b[i])
@@ -171,14 +171,15 @@ while cap.isOpened():
             if p_fv > 0.7 :
                 n_o += 1
                 positions.append(i)
-                cv2.putText(frame, f'{round(delta_v, 1)}', BOIs_coor[i][1],cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2 )
-                if lambda_f[i] < 2000:
-                    lambda_f[i] = (1 - lr_f[i]) * lambda_f[i] + lr_f[i] * delta_v
+                cv2.putText(frame, f'{round(var, 1)} - {round(g_var[i], 1)}', BOIs_coor[i][1],cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2 )
+                # if lambda_f[i] < 2000:
+                #     lambda_f[i] = (1 - lr_f[i]) * lambda_f[i] + lr_f[i] * delta_v
             elif p_f[i] < 0.5 or i == 0:
                 update_model(i, delta_v, mean)
-                if get_Vov(BOIs_var[i]) < 100:
-                    g_var[i] = 0.01 * var + (1 - 0.01)*g_var[i]
-                    g_mean[i] = 0.01 * mean + (1 - 0.01)*g_mean[i]
+                cv2.putText(frame, f'{round(var, 1)} - {round(g_var[i], 1)}', BOIs_coor[i][1],cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2 )
+                # if get_Vov(BOIs_var[i]) < 100:
+                    # g_var[i] = 0.01 * var + (1 - 0.01)*g_var[i]
+                    # g_mean[i] = 0.01 * mean + (1 - 0.01)*g_mean[i]
             else:
                 if p_f[i] > 0.5 and proba_mean(mean, g_mean[i], g_var[i],lr_am[i]) > proba_mean(g_mean[i] + 3*g_var[i], g_mean[i], g_var[i], lr_am[i]):
                     n_o +=1
@@ -192,11 +193,11 @@ while cap.isOpened():
         rate = round(n_o/len((BOIs_coor)), 2)
         cv2.putText(frame, f'{rate * 100}%', (100, 100),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2 )
         draw_BOI(frame, BOIs_coor, positions)
-    cv2.imshow("video",img)
-    out.write(frame)
+        cv2.imshow("video",frame)
+        out.write(frame)
     if cv2.waitKey(1) == ord('q'):
         break
-
+print(f"BOI_coors - {BOIs_coor}")
 cap.release()
 out.release()
 cv2.destroyAllWindows()
