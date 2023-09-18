2d721766833ed827a51173fc5e494e357b103597
dinhdungz <nguyendinhdung17062001@gmail.com>
2023-09-18 20:02:08 +0700

    update

--- draf.py
+++ draf.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import math
 import get_ROI as ROI
-import get_BOI as BOI
+import get_BOI as BOI 
 import time
 
 # Imread video
@@ -11,16 +11,16 @@ cap = cv2.VideoCapture('./Data Road Traffic/normal_3.mp4')
 # Setup video writer
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-fps = cap.get(cv2.CAP_PROP_FPS)
+fps =  cap.get(cv2.CAP_PROP_FPS)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output_2.avi', fourcc, fps, (width, height))
+out = cv2.VideoWriter('output_2.avi', fourcc, fps, (width,height))
 
 # Number frame in init background
-N = 6
+N = 6 
 N_BOI = 20
 init_background = True
 gen_BOI = True
-# Block coordinates
+#Block coordinates
 BOIs_coor = []
 count = 0
 n = 0
@@ -31,44 +31,30 @@ BOIs_mean = [[0 for i in range(N_BOI)] for j in range(N)]
 s = 0
 
 # Hệ số lambda size N_block
-lambda_f = np.array([100 for i in range(N_BOI)], ndmin=2)
-lambda_b = np.array([100 for i in range(N_BOI)], ndmin=2)
-lr_f = np.array([0.01 for i in range(N_BOI)], ndmin=2)
-lr_b = np.array([0.01 for i in range(N_BOI)], ndmin=2)
-lr_am = np.array([0.01 for i in range(N_BOI)], ndmin=2)
-p_f = np.array([0.4 for i in range(N_BOI)], ndmin=2)
+lambda_f = np.array([100 for i in range(N_BOI)], ndmin = 2)
+lambda_b = np.array([100 for i in range(N_BOI)], ndmin = 2)
+lr_f = np.array([0.01 for i in range(N_BOI)], ndmin = 2)
+lr_b = np.array([0.01 for i in range(N_BOI)], ndmin = 2)
+lr_am = np.array([0.01 for i in range(N_BOI)], ndmin = 2)
+p_f = np.array([0.4 for i in range(N_BOI)], ndmin = 2)
 
 # Mean and var for classify object
-g_mean = np.zeros((20, 1))
-g_var = np.zeros((20, 1))
-
+g_mean = np.zeros((20,1))
+g_var = np.zeros((20,1))
 
 def draw_BOI(img, coordinates, filter):
     # draw block
     for i in range(len(filter)):
-        if filter[i]:
-            cv2.rectangle(img, coordinates[i][0],
-                          coordinates[i][1], (0, 0, 255), 1)
+        if filter[i] == True:
+            cv2.rectangle(img, coordinates[i][0], coordinates[i][1], (0,0,255), 1)
         else:
-            cv2.rectangle(img, coordinates[i][0],
-                          coordinates[i][1], (0, 255, 0), 1)
-
+            cv2.rectangle(img, coordinates[i][0], coordinates[i][1], (0,255,0), 1)
 
 def write_text(img, filter, vars, g_var):
     # filter = np.reshape(filter,(1,N_BOI))
     for i in range(len(filter)):
-        if filter[i]:
-            cv2.putText(
-                img,
-                f'{int(vars[i])} - {int(g_var[i])}',
-                BOIs_coor[i][1],
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (255,
-                 0,
-                 0),
-                2)
-
+        if filter[i] == True:
+            cv2.putText(img, f'{int(vars[i])} - {int(g_var[i])}', BOIs_coor[i][1],cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2 )
 
 def get_mean_var(img, coordinates_block):
     # return mean, variance of blocks
@@ -82,20 +68,17 @@ def get_mean_var(img, coordinates_block):
         vars[i] = np.var(img[x_min: x_max, y_min: y_max])
     return means, vars
 
-
 def get_Vov(vars):
     # return variance of variance of block with some frame
-    Vov = np.var(vars, axis=0)
+    Vov = np.var(vars, axis = 0)
     return Vov
-
 
 def proba_mean(x, mean, var, lr):
     # return gau distribution
     mean = (1 - lr) * mean + lr * x
-    var = (1 - lr) * var + lr * (mean - x)**2
-    p = math.exp(-(x - mean)**2 / (2 * var)) / (math.sqrt(2 * math.pi * var))
+    var = (1 - lr) * var + lr * (mean - x)**2 
+    p = math.exp(-(x - mean)**2/(2*var))/(math.sqrt(2*math.pi*var))
     return p
-
 
 def update_model(i, delta_v, mean):
     if delta_v < lambda_b[i]:
@@ -104,24 +87,13 @@ def update_model(i, delta_v, mean):
         lr_b[i] = 0.1
     if lambda_b[i] < 500:
         lambda_b[i] = (1 - lr_b[i]) * lambda_b[i] + lr_b[i] * delta_v
-    if proba_mean(
-            mean,
-            BOIs_mean[i][0],
-            BOIs_var[i][0],
-            lr_am[i]) > proba_mean(
-            BOIs_mean[i][0] +
-            3 *
-            BOIs_var[i][0],
-            BOIs_mean[i][0],
-            BOIs_var[i][0],
-            lr_am[i]):
+    if proba_mean(mean, BOIs_mean[i][0], BOIs_var[i][0], lr_am[i]) > proba_mean(BOIs_mean[i][0] + 3*BOIs_var[i][0], BOIs_mean[i][0], BOIs_var[i][0], lr_am[i]):
         lr_am[i] = 0.01
     else:
         lr_am[i] = 0.1
-
+    
     g_mean[i] = (1 - lr_am[i]) * g_mean[i] + lr_am[i] * mean
-    g_var[i] = (1 - lr_am[i]) * g_var[i] + lr_am[i] * (mean - g_mean[i])**2
-
+    g_var[i] = (1 - lr_am[i]) * g_var[i] + lr_am[i]*(mean - g_mean[i])**2
 
 def update_p_f(positions, p_f):
     # upadate prior probability
@@ -133,6 +105,7 @@ def update_p_f(positions, p_f):
     return p_f
 
 
+
 while cap.isOpened():
     ret, frame = cap.read()
     # if frame is read correctly ret is True
@@ -140,9 +113,9 @@ while cap.isOpened():
         print("Can't receive frame (stream end?). Exiting ...")
         break
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    kernel = np.ones((5, 5), np.float32) / 25
-    img = cv2.filter2D(image, -1, kernel)
-    s += 1
+    kernel = np.ones((5,5),np.float32)/25
+    img = cv2.filter2D(image,-1,kernel)
+    s+=1
     n_o = 0
     # Generate ROI and BOI
     if gen_BOI:
@@ -152,7 +125,7 @@ while cap.isOpened():
         area = BOI.get_area(lanes)
         area_image = BOI.draw_lanes(lanes_image, area)
 
-        boi_image, BOIs_coor = BOI.get_BOI(area, area_image, N_BOI, 2)
+        boi_image, BOIs_coor = BOI.get_BOI(area, area_image,N_BOI, 2)
         cv2.imshow('lane', boi_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
@@ -161,18 +134,17 @@ while cap.isOpened():
     # Initialization background
     if init_background:
         if count < N:
-            means, vars = get_mean_var(img, BOIs_coor)
+            means, vars = get_mean_var(img,BOIs_coor)
             BOIs_mean[count] = np.reshape(means, (N_BOI,))
             BOIs_var[count] = np.reshape(vars, (N_BOI,))
-            count += 1
-            cv2.putText(frame, 'Init background', (100, 100),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            count +=1
+            cv2.putText(frame, 'Init background', (100, 100),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2 )
         else:
             pas = True
             Vov = get_Vov(BOIs_var)
-            means, vars = get_mean_var(img, BOIs_coor)
+            means, vars = get_mean_var(img,BOIs_coor)
             filter = Vov > 100
-            compare = filter
+            compare = filter == True
             if compare.any():
                 BOIs_mean.pop(0)
                 BOIs_var.pop(0)
@@ -181,15 +153,13 @@ while cap.isOpened():
                 BOIs_mean.append(np.reshape(means, (N_BOI,)))
                 pas = False
 
-            cv2.putText(frame, 'Init background', (100, 100),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(frame, 'Init background', (100, 100),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2 )
 
             if pas:
                 init_background = False
-                g_mean = np.mean(BOIs_mean, axis=0, keepdims=True)
-                g_var = np.mean(BOIs_var, axis=0, keepdims=True)
-                cv2.putText(frame, 'Done init background', (100, 100),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                g_mean = np.mean(BOIs_mean, axis = 0, keepdims= True)
+                g_var = np.mean(BOIs_var, axis = 0, keepdims= True)
+                cv2.putText(frame, 'Done init background', (100, 100),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2 )
 
     else:
         # Classify object
@@ -198,20 +168,19 @@ while cap.isOpened():
         means, vars = get_mean_var(img, BOIs_coor)
         delta_v = np.absolute(vars - g_var.T)
         delta_m = np.absolute(means - g_mean.T)
-        p_vb = np.exp(-delta_v / lambda_b.T)
-        p_vf = 1 - np.exp(-delta_v / lambda_f.T)
-        p_fv = (p_vf * p_f.T) / (p_vb * (1 - p_f.T) + p_vf * p_f.T)
-
+        p_vb = np.exp(-delta_v/ lambda_b.T)
+        p_vf = 1 - np.exp(-delta_v/lambda_f.T)
+        p_fv = (p_vf * p_f.T)/(p_vb * (1 - p_f.T) + p_vf * p_f.T)
+        
         f_1 = p_fv > 0.7
         # print(f_1)
         write_text(frame, f_1, vars, g_var.T)
         # if lambda_f[filter] < 2000:
         #     lambda_f[filter] = (1 - lr_f[filter]) * lambda_f[filter] + lr_f[filter] * delta_v[filter]
 
-        n_block = len(f_1[f_1])
-        rate = round(n_block / N_BOI, 1)
-        cv2.putText(frame, f'{rate * 100}%', (100, 100),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        n_block = len(f_1[f_1 == True])
+        rate = round(n_block/N_BOI,1)
+        cv2.putText(frame, f'{rate * 100}%', (100, 100),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2 )
         draw_BOI(frame, BOIs_coor, f_1)
         t2 = time.time()
         print(f"time - {t2-t1}")
@@ -253,7 +222,7 @@ while cap.isOpened():
         # t2 = time.time()
         # t = t2 - t1
         # print(f"time - {t}")
-    cv2.imshow("video", frame)
+    cv2.imshow("video",frame)
     out.write(frame)
     if cv2.waitKey(1) == ord('q'):
         break
--- util.py
+++ util.py
@@ -2,20 +2,15 @@ import numpy as np
 import cv2
 import math
 import get_ROI as ROI
-import get_BOI as BOI
-from vidgear.gears import WriteGear
+import get_BOI as BOI 
 
-
-def draw_BOI(img, coordinates, positions):
+def draw_BOI(img, coordinates, occupied):
     # draw block
     for i in range(len(coordinates)):
-        if i in positions:
-            cv2.rectangle(img, coordinates[i][0],
-                          coordinates[i][1], (0, 0, 255), 1)
+        if i in occupied:
+            cv2.rectangle(img, coordinates[i][0], coordinates[i][1], (0,0,255), 1)
         else:
-            cv2.rectangle(img, coordinates[i][0],
-                          coordinates[i][1], (0, 255, 0), 1)
-
+            cv2.rectangle(img, coordinates[i][0], coordinates[i][1], (0,255,0), 1)
 
 def get_mean_variance(img, coordinate_block):
     # return mean, variance of block
@@ -26,42 +21,52 @@ def get_mean_variance(img, coordinate_block):
     variance = np.var(img[x_min: x_max, y_min: y_max])
     return mean, variance
 
-
 def get_Vov(list_var):
     # return variance of variance of block with some frame
     Vov = np.var(list_var)
     return Vov
 
-
-def proba_mean(x, mean, var, lr):
+def mean_model(x, g_mean, g_var):
     # return gau distribution
-    mean = (1 - lr) * mean + lr * x
-    var = (1 - lr) * var + lr * (mean - x)**2
-    p = math.exp(-(x - mean)**2 / (2 * var)) / (math.sqrt(2 * math.pi * var))
+    p = math.exp(-(x - g_mean)**2/(2*g_var))/(math.sqrt(2*math.pi*g_var))
     return p
 
-
-def update_model(lambda_b, delta_v):
+def update_model(lambda_b , delta_v, mean, var, g_mean, g_var):
     # update lambda b, lr b
     if delta_v < lambda_b:
-        lr_b = 0.01
+        lr_b = 0.001
     else:
-        lr_b = 0.05
+        lr_b = 0.01
     if lambda_b < 500:
-        lambda_b = (1 - lr_b) * lambda_b + lr_b * delta_v
+        lambda_b = (1 - lr_b) * lambda_b + lr_b* delta_v
+    
+    if mean_model(mean, g_mean, g_var) < mean_model(g_mean + 3*g_var, g_mean, g_var):
+        lr_am = 0.01
+    else:
+        lr_am = 0.1
+    
+    g_mean = (1 - lr_am)*g_mean + lr_am * mean
+    g_var = (1 - lr_am)*g_var + lr_am * var #(mean - g_mean)**2
 
-    return lambda_b, lr_b
+    return lambda_b, g_mean, g_var
 
-
-def update_p_f(positions, p_f):
+def update_pf(occupied, p_f, direct = 'front'):
     # upadate prior probability
-    for pos in positions:
-        p_f[pos] = 0.5
-        if pos + 1 < len(p_f):
-            p_f[pos + 1] = 0.6
-    p_f[0] = 0.5
+    if direct == 'front':
+        for pos in occupied:
+            p_f[pos] = 0.5
+            if pos + 1 < len(p_f):
+                p_f[pos + 1] = 0.6
+        p_f[0] = 0.5
+        
+    else:
+        for pos in occupied:
+            p_f[pos] = 0.5
+            if pos > 0:
+                p_f[pos-1] = 0.6
+        p_f[-1] = 0.5
+    
     return p_f
-
 
 def gen_boi(frame, N_BOI, increment):
     # generate blocks
@@ -70,13 +75,12 @@ def gen_boi(frame, N_BOI, increment):
 
     area = BOI.get_area(lanes)
 
-    boi_image, BOIs_coor = BOI.get_BOI(area, lanes_image, N_BOI, increment)
-
+    boi_image, BOIs_coor = BOI.get_BOI(area, lanes_image,N_BOI, increment)
+    
     cv2.imshow('lane', boi_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     return BOIs_coor
-
 
 def create_3d_list(x, y, z):
     lst = []
@@ -90,37 +94,30 @@ def create_3d_list(x, y, z):
         lst.append(lst_2d)
     return lst
 
-
 def view_density(frame, density):
-    total_rate = (sum(density) / len(density))
+    total_rate = (sum(density)/len(density))
     y_coor = 100
     height, width, channels = np.shape(frame)
     for i in range(len(density)):
-        cv2.putText(
-            frame,
-            f'Lane {i + 1}: {density[i]}%',
-            (100,
-             y_coor),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.5,
-            (0,
-             255,
-             0),
-            2)
+        cv2.putText(frame, f'Lane {i + 1}: {density[i]}%', (100, y_coor),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2 )
         y_coor += 20
-    cv2.putText(frame, f'Total: {int(total_rate)}%', (int(
-        width / 2), 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
+    cv2.putText(frame, f'Total: {int(total_rate)}%', (int(width/2) - 50, 50),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2) 
 
 def estimate(video_path, N_BOI, N, increment):
     # Read video
     cap = cv2.VideoCapture(video_path)
 
-    # Define writer with output filename 'Output.mp4'
-    out = WriteGear(output='Output.mp4')
+    # Setup video writer
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    fps =  cap.get(cv2.CAP_PROP_FPS)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi', fourcc, fps, (width,height))
 
+    # Number frame in init background
     init_background = True
 
+    ret, frame = cap.read()
     ret, frame = cap.read()
 
     BOIs_coor = gen_boi(frame, N_BOI, increment)
@@ -129,12 +126,11 @@ def estimate(video_path, N_BOI, N, increment):
     # Mean and var blocks for init background size N x N_ Block
     BOIs_var = create_3d_list(N_lane, N_BOI, N)
     BOIs_mean = create_3d_list(N_lane, N_BOI, N)
+    
 
     # lambda size N_lane X N_block
     lambda_f = [[100 for i in range(N_BOI)] for j in range(N_lane)]
     lambda_b = [[100 for i in range(N_BOI)] for j in range(N_lane)]
-    lr_f = [[0.01 for i in range(N_BOI)] for j in range(N_lane)]
-    lr_b = [[0.01 for i in range(N_BOI)] for j in range(N_lane)]
     p_f = [[0.4 for i in range(N_BOI)] for j in range(N_lane)]
 
     # Mean and var for classify object
@@ -156,8 +152,9 @@ def estimate(video_path, N_BOI, N, increment):
             print("Can't receive frame (stream end?). Exiting ...")
             break
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        kernel = np.ones((5, 5), np.float32) / 25
-        img = cv2.filter2D(image, -1, kernel)
+        kernel = np.ones((5,5),np.float32)/25
+        img = cv2.filter2D(image,-1,kernel)
+        
 
         # Initialization background
         if init_background:
@@ -168,8 +165,7 @@ def estimate(video_path, N_BOI, N, increment):
                         BOIs_var[lane][i][count] = var
                         BOIs_mean[lane][i][count] = mean
                 count += 1
-                cv2.putText(frame, 'Init background', (100, 100),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                cv2.putText(frame, 'Init background', (100, 100),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2 )
             else:
                 pas = True
                 for lane in range(N_lane):
@@ -181,64 +177,58 @@ def estimate(video_path, N_BOI, N, increment):
                             pas = False
                             BOIs_var[lane][i].pop(0)
                             BOIs_mean[lane][i].pop(0)
-                            mean, var = get_mean_variance(
-                                img, BOIs_coor[lane][i])
+                            mean, var = get_mean_variance(img, BOIs_coor[lane][i])
                             BOIs_var[lane][i].append(var)
                             BOIs_mean[lane][i].append(mean)
-                cv2.putText(frame, 'Init background', (100, 100),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
+                cv2.putText(frame, 'Init background', (100, 100),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2 )
+                
                 if pas:
                     init_background = False
-                    g_mean = np.mean(BOIs_mean, axis=2)
-                    g_var = np.mean(BOIs_var, axis=2)
-
-                    cv2.putText(frame, 'Done init background', (100, 100),
-                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    g_mean = np.mean(BOIs_mean, axis = 2)
+                    g_var = np.mean(BOIs_var, axis = 2)
+                    
+                    cv2.putText(frame, 'Done init background', (100, 100),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2 )
 
         else:
             # Classify object
-
+            
             density = []
             for lane in range(N_lane):
                 n = 0
-                positions = []
+                occupied = []
                 for i in range(N_BOI):
                     # cv2.putText(frame, str(i), BOIs_coor[i][0],cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2 )
                     mean, var = get_mean_variance(img, BOIs_coor[lane][i])
                     delta_v = abs(var - g_var[lane][i])
-                    delta_m = abs(mean - g_mean[lane][i])
-                    p_vb = math.exp(-delta_v / lambda_b[lane][i])
-                    p_vf = 1 - math.exp(-delta_v / lambda_f[lane][i])
-                    p_fv = (p_vf * p_f[lane][i]) / (p_vb * \
-                            (1 - p_f[lane][i]) + p_vf * p_f[lane][i])
-                    if p_fv > 0.7:
+                    # delta_m = abs(mean - g_mean[lane][i])
+                    p_vb = math.exp(-delta_v/lambda_b[lane][i])
+                    p_vf = 1 - math.exp(-delta_v/lambda_f[lane][i])
+                    p_fv =  (p_vf * p_f[lane][i])/(p_vb * (1 - p_f[lane][i]) + p_vf * p_f[lane][i])
+                    if p_fv > 0.7 :
                         n += 1
-                        positions.append(i)
-                        # cv2.putText(frame, f'{int(var)} - {int(g_var[lane][i])}', BOIs_coor[lane][i][1],cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2 )
-                        if lambda_f[lane][i] < 2000:
-                            lambda_f[lane][i] = (
-                                1 - lr_f[lane][i]) * lambda_f[lane][i] + lr_f[lane][i] * delta_v
-                    elif p_f[lane][i] < 0.5 and p_fv < 0.5:
-                        lambda_b[lane][i], lr_b[lane][i] = update_model(
-                            lambda_b[lane][i], delta_v)
+                        occupied.append(i) #int(var)} - {int(g_var[lane][i])
+                        cv2.putText(frame, f'{int(mean)} - {int(g_mean[lane][i])} ', BOIs_coor[lane][i][1],cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2 )
+                        if lambda_f[lane][i] < 500:
+                            lambda_f[lane][i] = (1 - 0.01) * lambda_f[lane][i] + 0.01 * delta_v
+                    elif p_f[lane][i] < 0.5:
+                        lambda_b[lane][i], g_mean[lane][i], g_var[lane][i] = update_model(lambda_b[lane][i], delta_v, mean,var, g_mean[lane][i], g_var[lane][i])
                     else:
-                        if delta_m > 20:
-                            n += 1
+                        if p_f[lane][i] > 0.5 and (mean_model(mean, g_mean[lane][i], g_var[lane][i] ) < mean_model(g_mean[lane][i] + 3*g_var[lane][i] , g_mean[lane][i], g_var[lane][i]) ) :
+                            n +=1
                             # cv2.putText(frame, f'{int(delta_m)}', BOIs_coor[lane][i][1],cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2 )
-                            positions.append(i)
+                            # cv2.putText(frame, f'{int(var)} - {int(g_var[lane][i])}', BOIs_coor[lane][i][1],cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2 )
+                            occupied.append(i)
                         else:
-                            lambda_b[lane][i], lr_b[lane][i] = update_model(
-                                lambda_b[lane][i], delta_v)
-
-                d_lane = int(n * 100 / N_BOI)
+                            lambda_b[lane][i], g_mean[lane][i], g_var[lane][i] = update_model(lambda_b[lane][i], delta_v, mean,var, g_mean[lane][i], g_var[lane][i])
+                    
+                d_lane = int(n * 100/N_BOI) 
                 density.append(d_lane)
-                draw_BOI(frame, BOIs_coor[lane], positions)
-
+                draw_BOI(frame, BOIs_coor[lane], occupied)
+            
                 p_f[lane] = [0.4 for i in range(N_BOI)]
-                p_f[lane] = update_p_f(positions, p_f[lane])
+                p_f[lane] = update_pf(occupied, p_f[lane])
 
-            rate = int(sum(density) / len((density)))
+            rate = int(sum(density)/len((density)))
             if n_frame % 20 == 0:
                 v_density = density
             view_density(frame, v_density)
@@ -250,14 +240,14 @@ def estimate(video_path, N_BOI, N, increment):
             else:
                 n_light += 1
 
-        cv2.imshow("video", frame)
+        cv2.imshow("video",frame)
         out.write(frame)
         if cv2.waitKey(1) == ord('q'):
             break
-
+ 
     if n_frame == 0:
-        return
-
+        return 
+    
     if max(n_light, n_medium, n_heavy) == n_heavy:
         result = 'heavy'
     elif max(n_light, n_medium, n_heavy) == n_medium:
@@ -266,6 +256,12 @@ def estimate(video_path, N_BOI, N, increment):
         result = 'light'
 
     cap.release()
-    out.close()
+    out.release()
     cv2.destroyAllWindows()
     return result
+
+if __name__ == "__main__":
+    pf = [0.5, 0.5, 0.5, 0.5, 0.5]
+    occupied = [0,1]
+    pf = update_pf(occupied, pf)
+    print(pf)
